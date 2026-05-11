404cf107d99b0a52fbe716dff3bdad33aea14988
aXel0ide Clase <diezalex587@gmail.com>
2026-05-11 19:43:45 +0200

    Creacion de estilos y añadir el enlace

--- gestor-personajes-frikis/index.php
+++ gestor-personajes-frikis/index.php
@@ -105,6 +105,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestor de Personajes Frikis</title>
+    <link rel="stylesheet" href="./styles.css">
 </head>
 <body>
     <header>
