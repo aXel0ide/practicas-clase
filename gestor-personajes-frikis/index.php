300544df7463c68ac9a8627eca430a1fe97b23cf
aXel0ide Clase <diezalex587@gmail.com>
2026-05-11 17:44:36 +0200

    Crear estructura de index.php junto con un array con personajes de comics

---
+++ gestor-personajes-frikis/index.php
@@ -0,0 +1,63 @@
+<?php
+    $personajes = [
+        [
+            "nombre" => "Batman",
+            "universo" => "DC",
+            "tipo" => "Héroe",
+            "poder" => "Inteligencia, habilidades de combate, tecnología avanzada",
+            "anio" => 1939,
+            "imagen" => "./img/batman.jpg",
+            "activo" => true
+        ],
+        [
+            "nombre" => "Spider-Man",
+            "universo" => "Marvel",
+            "tipo" => "Héroe",
+            "poder" => "Fuerza, agilidad, habilidades de trepador, sentido arácnido",
+            "anio" => 1962,
+            "imagen" => "./img/spiderman.jpg",
+            "activo" => true
+        ],
+        [
+            "nombre" => "Loki",
+            "universo" => "Marvel",
+            "tipo" => "Villano",
+            "poder" => "Magia, manipulación, astucia",
+            "anio" => 1949,
+            "imagen" => "./img/loki.jpg",
+            "activo" => false
+        ],
+        [
+            "nombre" => "Superlópez",
+            "universo" => "Bruguera",
+            "tipo" => "Héroe",
+            "poder" => "Superfuerza, supervelocidad, vuelo",
+            "anio" => 1973,
+            "imagen" => "./img/superlopez.jpg",
+            "activo" => true
+        ]
+    ];
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestor de Personajes Frikis</title>
+</head>
+<body>
+    <header>
+        <h1>Gestor de Personajes Frikis</h1>
+        <p>Bienvenido al gestor de personajes frikis.</p>
+    </header>
+    <nav>
+
+    </nav>
+    <main>
+        
+    </main>
+    <footer>
+        <p>&copy; 2025 Gestor de Personajes Frikis. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
