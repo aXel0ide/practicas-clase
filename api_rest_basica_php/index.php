a260d810921f846e56627a9287b61abeeb1b9735
aXel0ide Clase <diezalex587@gmail.com>
2026-06-17 20:41:17 +0200

    Formato de index.php

---
+++ api_rest_basica_php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio - API REST básica</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+    <header>
+        <h1>Mini aplicación con API REST básica</h1>
+        <p>Práctica de UF1846: servicio, parámetros GET, filtros y consumo de datos.</p>
+    </header>
+    <main class="contenedor-inicio">
+        <a class="boton" href="./api_peliculas.php">Ver API completa</a>
+        <a class="boton" href=""></a>
+        <a class="boton" href=""></a>
+        <a class="boton" href=""></a>
+    </main>
+</body>
+</html>
