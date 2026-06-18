e5a97d1821801d9491fd0278e24b1a397bb869f3
aXel0ide Clase <diezalex587@gmail.com>
2026-06-18 20:24:39 +0200

    Crear servicio_peliculas.php y consumir_servicio.php

---
+++ servicio_web_php_y_consumo_de_datos/consumir_servicio.php
@@ -0,0 +1,40 @@
+<?php
+    $url = "http://localhost/practicas-clase/servicio_web_php_y_consumo_de_datos/servicio_peliculas.php";
+
+    $respuesta = file_get_contents($url);
+
+    $peliculas = json_decode($respuesta, true);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consumo de servicio de películas</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+    <header>
+        <h1>Peliculas basadas en cómics</h1>
+        <p>Datos obtenidos desde un servicio PHP en formato JSON.</p>
+    </header>
+    <main class="contenedor">
+        <?php 
+            foreach($peliculas as $pelicula){
+                echo "
+                    <article class='tarjeta'>
+                        <img src='" . $pelicula["imagen"] . "' alt='" . $pelicula["titulo"] . "'>
+                        <h2>" . $pelicula["titulo"] . "</h2>
+                        <p><strong>Universo: </strong>" . $pelicula["universo"] . "</p>
+                        <p><strong>Año: </strong>" . $pelicula["anio"] . "</p>
+                        <p><strong>Director: </strong>" .$pelicula["director"] . "</p>
+                        <p><strong>Formato: </strong>" . $pelicula["formato"] . "</p>
+                        <p>" . $pelicula["sinopsis"] . "</p>
+                    </article>
+                ";
+            }
+        ?>
+    </main>
+</body>
+</html>
