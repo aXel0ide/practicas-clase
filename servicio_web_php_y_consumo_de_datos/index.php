353e95ef85c5440fb6c6794738656d6a981a7cf1
aXel0ide Clase <diezalex587@gmail.com>
2026-06-19 17:01:04 +0200

    Crear un index.php con enlaces apuntando a todos los archivos de la práctica

---
+++ servicio_web_php_y_consumo_de_datos/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio - Mini aplicación de películas</title>
+</head>
+<body>
+    <header>
+        <h1>Mini aplicación de películas basadas en cómics</h1>
+    </header>
+    <main class="contenedor-inicio">
+        <a href="./servicio_peliculas.php" class="boton-enlace">Vers servicio JSON</a>
+        <a href="./consumir_servicio.php" class="boton-enlace">Ver catálogo completo</a>
+        <a href="./espanolas.php" class="boton-enlace">Ver películas españolas</a>
+        <a href="./modernas.php" class="boton-enlace">Ver películas mordernas</a>
+    </main>
+</body>
+</html>
